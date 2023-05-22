6129b545735cfead3814a6576c3eb36280e9e19e
sjcy2704 <a1834160@adelaide.edu.au>
2023-05-22 17:48:32 +0930

    Add klargest

---
+++ klargest.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+int kth_largest(std::vector<int> values, int k) {
+  std::priority_queue<int> pq;
+  int size = values.size();
+
+  for (int i = 0; i < size; i++) {
+    pq.push(values.at(i));
+  }
+
+  for (int i = 0; i < k; i++) {
+    pq.pop();
+  }
+
+  return pq.top();
+}
